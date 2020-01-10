df8cad20357337f26aa2daf9bcda64b54f784a11
zgorzk <36644924+zgorzk@users.noreply.github.com>
2020-01-10 22:57:26 +0100

    Add files via upload

---
+++ WindowsFormsApp6/WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Bconnect = new System.Windows.Forms.Button();
+            this.Bdisconnect = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.RTBLogBox = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.BsendFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Bconnect
+            // 
+            this.Bconnect.Location = new System.Drawing.Point(25, 95);
+            this.Bconnect.Name = "Bconnect";
+            this.Bconnect.Size = new System.Drawing.Size(177, 39);
+            this.Bconnect.TabIndex = 0;
+            this.Bconnect.Text = "CONNECT: COM1 ";
+            this.Bconnect.UseVisualStyleBackColor = true;
+            this.Bconnect.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Bdisconnect
+            // 
+            this.Bdisconnect.Location = new System.Drawing.Point(25, 140);
+            this.Bdisconnect.Name = "Bdisconnect";
+            this.Bdisconnect.Size = new System.Drawing.Size(177, 39);
+            this.Bdisconnect.TabIndex = 1;
+            this.Bdisconnect.Text = "DISCONNECT";
+            this.Bdisconnect.UseVisualStyleBackColor = true;
+            this.Bdisconnect.Click += new System.EventHandler(this.Bdisconnect_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(53, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "COM1";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // RTBLogBox
+            // 
+            this.RTBLogBox.Location = new System.Drawing.Point(215, 209);
+            this.RTBLogBox.Name = "RTBLogBox";
+            this.RTBLogBox.Size = new System.Drawing.Size(476, 202);
+            this.RTBLogBox.TabIndex = 3;
+            this.RTBLogBox.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(268, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "KOMENDA HAYES\'A: ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(445, 69);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(227, 26);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.Enter += new System.EventHandler(this.textBox2_Enter);
+            this.textBox2.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyUp);
+            // 
+            // BsendFile
+            // 
+            this.BsendFile.Location = new System.Drawing.Point(289, 111);
+            this.BsendFile.Name = "BsendFile";
+            this.BsendFile.Size = new System.Drawing.Size(208, 39);
+            this.BsendFile.TabIndex = 6;
+            this.BsendFile.Text = "send file";
+            this.BsendFile.UseVisualStyleBackColor = true;
+            this.BsendFile.Click += new System.EventHandler(this.BsendFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BsendFile);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.RTBLogBox);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Bdisconnect);
+            this.Controls.Add(this.Bconnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Bconnect;
+        private System.Windows.Forms.Button Bdisconnect;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox RTBLogBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button BsendFile;
+    }
+}
+
---
+++ WindowsFormsApp6/WindowsFormsApp6/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using System.IO;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        string filename;
+        static SerialPort _serialPort;
+        Thread read;
+        public void sendMessage(string message)
+        {
+            if (_serialPort != null)
+            {
+                _serialPort.Write(message + Environment.NewLine);
+            }
+            else
+            {
+                RTBLogBox.Text += "nie wykryto podłaczonego modemu\n";
+            }
+        }
+        public void Read()
+        {
+            while (_serialPort.IsOpen)
+            {
+                try
+                {
+                    string message = _serialPort.ReadExisting();
+                    if (message.Length > 0)
+                    {
+                        Console.Write(message);
+                    }
+                }
+                catch (TimeoutException) { }
+            }
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (_serialPort != null)
+            {
+                if (_serialPort.IsOpen) _serialPort.Close();
+            }
+            _serialPort = new SerialPort(textBox1.Text);
+
+            // Allow the user to set the appropriate properties.
+            _serialPort.BaudRate = 9600;
+            _serialPort.DtrEnable = true;
+            _serialPort.Handshake = Handshake.RequestToSendXOnXOff;
+
+            // Set the read/write timeouts
+            _serialPort.ReadTimeout = 500;
+            _serialPort.WriteTimeout = 500;
+
+            if (_serialPort != null)
+            {
+                _serialPort.Open();
+            }
+            RTBLogBox.Text += ("connected to port " + textBox1.Text + "\n");
+            this.sendMessage("ATZ"); // resetuje
+            Thread.Sleep(500);
+            this.sendMessage("ATE1"); // Echo commands to computer
+
+
+            read = new Thread(Read);
+            read.Start();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Bconnect.Text = "CONNECT: " + textBox1.Text;
+        }
+
+        private void Bdisconnect_Click(object sender, EventArgs e)
+        {
+            if (_serialPort != null)
+            {
+                RTBLogBox.Text += "rozłaczono\n";
+                _serialPort.Close();
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (_serialPort != null)
+                {
+                    this.sendMessage(textBox2.Text);
+                    RTBLogBox.Text += "wysłano wiadomośc" + textBox2.Text + "\n";
+                }
+                Thread.Sleep(500);
+            }
+
+        }
+
+        private void BsendFile_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                filename = openFileDialog.FileName;
+            }
+            FileStream fileStream = File.OpenRead(filename);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            _serialPort.Write(binaryReader.ReadBytes((int)fileStream.Length), 0, (int)fileStream.Length);
+        }
+
+    }
+}
